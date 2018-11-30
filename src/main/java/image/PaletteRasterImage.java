3587f1079f46a7397a8007365f2b2e564f669fd3
LauraJeanneton <laura.jeanneton@etu.univ-amu.fr>
2018-11-30 11:27:38 +0100

    Tache 2 : constructeurs paletteRasterImage ,methodes ,  Enum RasterImageType, modifications dans display.

--- src/main/java/image/PaletteRasterImage.java
+++ src/main/java/image/PaletteRasterImage.java
@@ -29,28 +29,55 @@ public class PaletteRasterImage implements Image {
         this.setHeight(Matrices.getColumnCount(pixels));
         this.setWidth(Matrices.getRowCount(pixels));
         createRepresentation();
+        System.out.println(pixels.length);
+        System.out.println(pixels[0].length);
+
 
         for (int i = 0; i < this.width; i++) {
             for (int j = 0; j < this.height; j++) {
-                if (palette.indexOf(pixels[i][j]) != -1) {
-                    palette.add(pixels[i][j]);
-                    indexesOfColors[i][j] = indexesOfColors.length -1;
+
+                setPixelColor(pixels[i][j],i,j);
+
+
                 }
-                else indexesOfColors[i][j] =palette.indexOf(pixels[i][j]);
             }
         }
-    }
+
 
     public void createRepresentation(){
         this.palette = new ArrayList<>();
-        this.indexesOfColors = new int[this.height][this.width];
+        this.indexesOfColors = new int[this.width][this.height];
     }
 
-
-    @Override
-    public Color getPixelColor(int x, int y) {
-        return null;
+    public void setPixelsColor(Color color){
+        palette.add(color);
+        for(int i = 0 ; i < width ; i++ ){
+            for(int j =  0 ; j < height ; j++){
+                indexesOfColors[i][j] = 0;
+            }
+        }
     }
+    public void setPixelsColor(Color[][] pixels){
+        for (int i = 0; i < this.width; i++) {
+            for (int j = 0; j < this.height; j++) {
+                setPixelColor(pixels[i][j] , i , j);
+            }
+        }
+    }
+    public void setPixelColor(Color color, int x, int y){
+        if(!palette.contains(color)) {
+            palette.add(color);
+            indexesOfColors[x][y] = palette.indexOf(color);
+        }
+        else indexesOfColors[x][y] = palette.indexOf(color);
+
+    }
+
+    public Color getPixelColor(int x, int y){
+        int index = indexesOfColors[x][y];
+        return palette.get(index);
+    }
+
 
     @Override
     public int getWidth() {
