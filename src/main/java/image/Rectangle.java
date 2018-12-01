ab0d71ee7133ee02e7150d3af7eef431feaee1d4
Fayçal <faycal.henis@yahoo.fr>
2018-12-01 20:49:24 +0100

    Classe Rectangle et methodes

---
+++ src/main/java/image/Rectangle.java
@@ -0,0 +1,35 @@
+package image;
+
+import javafx.scene.paint.Color;
+
+public class Rectangle implements Shape {
+
+    public int x;
+    public int y;
+    public int width;
+    public int height;
+    public Color color;
+
+
+    public Rectangle(int x , int y , int width , int height, Color color){
+
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+
+    }
+
+    @Override
+    public boolean contains(Point point) {
+         return this.x <= point.x && point.x <= this.x + this.width && this.y <= point.y &&
+                point.y <= this.y + this.height;
+    }
+
+    @Override
+    public Color getColor() {
+
+        return this.color;
+    }
+}
